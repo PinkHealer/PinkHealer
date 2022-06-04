caa66a481bdd12baebea51c811d407d3a68cdf6d
Parabeac-Nest <admin@parabeac.com>
2022-06-04 15:46:28 +0000

    Auto-generated commit by Parabeac Nest

---
+++ lib/views/page_1/i_phone13_pro_max_7.g.dart
@@ -0,0 +1,114 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:auto_size_text/auto_size_text.dart';
+
+
+class IPhone13ProMax7 extends StatefulWidget{
+  
+  
+  const IPhone13ProMax7( {Key? key, }) : super(key: key);
+  @override
+  _IPhone13ProMax7 createState() => _IPhone13ProMax7();
+}
+
+class _IPhone13ProMax7 extends State<IPhone13ProMax7>{
+  
+  _IPhone13ProMax7();
+
+  @override
+  Widget build(BuildContext context){
+    
+    return Material(
+color: Colors.white,
+child: Stack(
+children: [Positioned(left: 0,width: 750.0,top: 0,height: 1153.0,child: Image.asset('assets/images/whatsappimage20220520at11251.png', package: 'servihogar_f',width: 750.000,height: 1153.000, fit: BoxFit.none,),),Positioned(left: 574.0,width: 40.0,top: 117.0,height: 40.0,child: Container(width: 40.000,height: 40.000,decoration: BoxDecoration(),child: Stack(
+children: [Positioned(left: 0,width: 40.0,top: 0,height: 40.0,child: Container(width: 40.000,height: 40.000,decoration: BoxDecoration(),child: Stack(
+children: [Positioned(left: 0,width: 40.0,top: 0,height: 40.0,child: Container(width: 40.000,height: 40.000,decoration: BoxDecoration(color: Color(0x3dffffff),
+borderRadius: BorderRadius.all(Radius.circular(14)),),),),Positioned(left: 17.0,width: 6.0,top: 14.0,height: 12.0,child: SvgPicture.asset('assets/images/vector.svg', package: 'servihogar_f',width: 6.000,height: 12.000, fit: BoxFit.none,),),])),),])),),Positioned(left: 283.0,width: 412.0,top: 864.0,height: 75.0,child: SvgPicture.asset('assets/images/rectangle7.svg', package: 'servihogar_f',width: 412.000,height: 75.000, fit: BoxFit.none,),),Positioned(left: 237.0,width: 46.0,top: 106.0,height: 53.0,child: Container(width: 46.000,height: 53.000,decoration: BoxDecoration(),child: Stack(
+children: [Positioned(left: 2.0,width: 42.0,top: 16.0,height: 37.0,child: Image.asset('assets/images/rectangle3.png', package: 'servihogar_f',width: 42.000,height: 37.000, fit: BoxFit.none,),),Positioned(left: 0,width: 46.0,top: 0,height: 46.0,child: Image.asset('assets/images/rectangle2.png', package: 'servihogar_f',width: 46.000,height: 46.000, fit: BoxFit.none,),),])),),Positioned(left: 276.0,width: 123.0,top: 211.0,height: 52.0,child: Container(width: 123.000,height: 52.000,child: AutoSizeText(
+'Bravo!',
+style: TextStyle(
+fontFamily: 'Red Hat Display',
+fontSize: 39.0625,
+fontWeight: FontWeight.w900,
+letterSpacing: 0,
+color: Color(0xfffaf9fe),
+),textAlign: TextAlign.center,
+
+)),),Positioned(left: 237.0,width: 386.001,top: 384.0,height: 386.001,child: Container(width: 386.001,height: 386.001,decoration: BoxDecoration(),child: Stack(
+children: [Positioned(left: 0,width: 386.001,top: 0,height: 386.001,child: Image.asset('assets/images/background.png', package: 'servihogar_f',width: 386.001,height: 386.001, fit: BoxFit.none,),),Positioned(left: 109.5,width: 169.0,top: 109.0,height: 169.0,child: Container(width: 169.000,height: 169.000,decoration: BoxDecoration(boxShadow: [            BoxShadow(
+              color : Color(0x05faf9fe),
+              spreadRadius: 5.921928882598877,
+              blurRadius: 5.921928882598877,
+              offset: Offset(0, 2.767256498336792),
+            ),
+                      BoxShadow(
+              color : Color(0x07faf9fe),
+              spreadRadius: 14.231218338012695,
+              blurRadius: 14.231218338012695,
+              offset: Offset(0, 6.650102138519287),
+            ),
+                      BoxShadow(
+              color : Color(0x09faf9fe),
+              spreadRadius: 26.79612159729004,
+              blurRadius: 26.79612159729004,
+              offset: Offset(0, 12.521552085876465),
+            ),
+                      BoxShadow(
+              color : Color(0x0bfaf9fe),
+              spreadRadius: 47.79970169067383,
+              blurRadius: 47.79970169067383,
+              offset: Offset(0, 22.3363094329834),
+            ),
+                      BoxShadow(
+              color : Color(0x0dfaf9fe),
+              spreadRadius: 89.40408325195312,
+              blurRadius: 89.40408325195312,
+              offset: Offset(0, 41.777610778808594),
+            ),
+                      BoxShadow(
+              color : Color(0x12faf9fe),
+              spreadRadius: 214,
+              blurRadius: 214,
+              offset: Offset(0, 100),
+            ),
+          ],),child: Stack(
+children: [Positioned(left: 0,width: 169.0,top: 0,height: 169.0,child: Image.asset('assets/images/3.png', package: 'servihogar_f',width: 169.000,height: 169.000, fit: BoxFit.none,),),Positioned(left: 0,width: 169.0,top: 0,height: 169.0,child: Image.asset('assets/images/2.png', package: 'servihogar_f',width: 169.000,height: 169.000, fit: BoxFit.none,),),Positioned(left: 0,width: 169.0,top: 0,height: 169.0,child: Image.asset('assets/images/1.png', package: 'servihogar_f',width: 169.000,height: 169.000, fit: BoxFit.none,),),Positioned(left: 50.037,width: 68.731,top: 41.311,height: 85.759,child: SvgPicture.asset('assets/images/no_name_1.svg', package: 'servihogar_f',width: 68.731,height: 85.759, fit: BoxFit.none,),),])),),])),),Positioned(left: 302.0,width: 259.0,top: 330.0,height: 64.0,child: Container(width: 259.000,height: 64.000,child: AutoSizeText(
+'Has completado un pedido: su pago está en el camino!',
+style: TextStyle(
+fontFamily: 'Red Hat Display',
+fontSize: 20,
+fontWeight: FontWeight.w500,
+letterSpacing: 0,
+color: Color(0xfffaf9fe),
+),textAlign: TextAlign.center,
+
+)),),Positioned(left: 320.0,width: 302.0,top: 877.0,height: 49.0,child: Container(width: 302.000,height: 49.000,child: AutoSizeText(
+'Paga tu servicio',
+style: TextStyle(
+fontFamily: 'Public Sans',
+fontSize: 36,
+fontWeight: FontWeight.w500,
+letterSpacing: 0,
+color: Colors.black,
+),textAlign: TextAlign.center,
+
+)),),]), 
+);
+  }
+
+      @override
+    void dispose() {
+      
+      super.dispose();
+    }
+    
+}
+
